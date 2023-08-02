c6fbc74bb5bb477fd70e0f596405708e44e58318
Ushnish Tapaswi <126304736+plushexe351@users.noreply.github.com>
2023-08-02 18:44:45 +0530

    Added hamburger menu support for responsive design

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const developerName = document.getElementById('developer-name');
+const header = document.querySelector('header');
+const hamburg = document.querySelector('.hamburg');
+const nav = document.querySelector('header nav');
+const navLinks = document.querySelector('.nav-items')
+
+developerName.onclick = () => window.location.reload();
+hamburg.addEventListener('click', () => {
+    header.classList.toggle('active');
+})
+
 var typed = new Typed('.role', {
     strings: ["UI/UX Designer", "Frontend Developer"],
     typeSpeed: 60,
